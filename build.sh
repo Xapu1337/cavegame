53c26f78b4eb482a92ee6af6d0cbaf1fe01f93df
Parax <xapursplaz@gmail.com>
2025-06-12 11:32:37 +0200

    Add SDL cube flop example

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 set -e
-
-# Simple build script that compiles build.c using gcc.
-# If mingw is available, you can cross-compile on Linux for Windows
-# by running build.bat under Wine or using x86_64-w64-mingw32-gcc.
-
 mkdir -p build
-gcc -std=c11 -g -O0 build.c -o build/game "$@"
+gcc -std=c11 app/sdl_cube_flop.c -lm $(sdl2-config --cflags --libs) -lGL -lGLU -o build/game "$@"
